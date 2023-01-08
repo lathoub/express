17dcdfde5d1ac964e82f81777fa58b248ee99975
lathoub <4082369+lathoub@users.noreply.github.com>
2023-01-08 08:10:51 +0100

    Update HelloWorld.ino

--- examples/HelloWorld/HelloWorld.ino
+++ examples/HelloWorld/HelloWorld.ino
@@ -3,7 +3,7 @@ using namespace EXPRESS_NAMESPACE;
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
-Express express;
+Express app;
 
 void setup() {
   Serial.begin(115200);
@@ -11,20 +11,16 @@ void setup() {
 
   Ethernet.begin(mac);
 
-  express.get("/", [](Request &req, Response &res) {
-    res.send("Hello World!"));
+  app.get(F("/"), [](Request &req, Response &res) {
+    res.send(F("Hello World!"));
   });
 
-  express.listen(3000, []() {
-<<<<<<< Updated upstream
-    Serial.print("Example app listening on port ");
-    Serial.println(express.port);
-=======
-    Serial.println("Webserver on IP: listening on port:", express.port);
->>>>>>> Stashed changes
+  app.listen(3000, []() {
+    Serial.print(F("Example app listening on port "));
+    Serial.println(app.port);
   });
 }
 
 void loop() {
-  express.run();
+  app.run();
 }
