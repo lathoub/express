e8608cfbda9a8119c794ea767c07e2c09319adf0
lathoub <4082369+lathoub@users.noreply.github.com>
2023-01-09 16:43:03 +0100

    added body-Parser WIP

---
+++ src/bodyparser.h
@@ -0,0 +1,23 @@
+#pragma once
+
+class bodyparser
+{
+public:
+    bool inflate;
+    String type;
+    String limit;
+
+public:
+    static int json()
+    {
+    }
+    static int urlencoded()
+    {
+    }
+    static int raw()
+    {
+    }
+    static int text()
+    {
+    }
+};
