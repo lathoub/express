4142c9636bed2c10b350e1b3b49893b7429d3107
lathoub <4082369+lathoub@users.noreply.github.com>
2023-01-15 12:50:44 +0100

    updated dictionary and vector

--- src/utility/dictionary.h
+++ src/utility/dictionary.h
@@ -2,27 +2,83 @@
 
 #include "vector.h"
 
-template <class T, class U, size_t max_size_ = 10>
+template <class T, class U>
+class dictionary_item
+{
+public:
+    T key;
+    U value;
+};
+
+template <class T, class U, size_t MaxSize>
+class dictionary_iterator
+{
+    typedef dictionary_item<T, U> dictionary_item;
+    typedef vector<dictionary_item, MaxSize> dictionary_items;
+    typedef dictionary_iterator<T, U, MaxSize> iterator;
+    typedef dictionary_iterator<T, U, MaxSize> const_iterator;
+
+public:
+    explicit dictionary_iterator(dictionary_items* kvps) : values_ptr_{ kvps }, position_{ 0 }
+    {
+    }
+
+    dictionary_iterator(dictionary_items* kvps, const size_t size) : values_ptr_{ kvps }, position_{ size }
+    {
+    }
+
+    bool operator!=(const_iterator& other) const
+    {
+        return !(*this == other);
+    }
+
+    bool operator==(const_iterator& other) const
+    {
+        return position_ == other.position_;
+    }
+
+    dictionary_iterator& operator++()
+    {
+        ++position_;
+        return *this;
+    }
+
+    dictionary_item& operator*() const
+    {
+        return values_ptr_->at(position_);
+    }
+
+private:
+    dictionary_items* values_ptr_;
+
+    size_t position_;
+};
+
+template <class T, class U, size_t MaxSize = 10>
 class dictionary
 {
-    vector<T, max_size_> keys_{};
-    vector<U, max_size_> values_{};
+    typedef dictionary_item<T, U> dictionary_item;
+    typedef vector<dictionary_item, MaxSize> dictionary_items;
+
+    dictionary_items kvps_{};
 
     auto get(T key) -> U&
     {
-        for (size_t i = 0; i < keys_.size(); i++)
+        for (size_t i = 0; i < kvps_.size(); i++)
         {
-            if (keys_.at(i) == key)
+            if (kvps_.at(i).key == key)
             {
-                return values_.at(i);
+                return kvps_.at(i).value;
             }
         }
 
-        // not found, so add it with empty value
-        keys_.push_back(key);
-        values_.push_back(U{});
+        dictionary_item item{};
+        item.key = key;
+        item.value = U{};
 
-        return values_.back();
+        kvps_.push_back(item);
+
+        return kvps_.back().value;
     }
 
 public:
@@ -38,6 +94,33 @@ public:
 
     auto length() -> size_t
     {
-        return keys_.size();
+        return kvps_.size();
     }
+
+public:
+
+    typedef dictionary_iterator<T,U, MaxSize> iterator;
+
+    iterator begin()
+    {
+        return iterator(&kvps_);
+    }
+
+    iterator end()
+    {
+        return iterator(&kvps_, kvps_.size());
+    }
+
+    typedef dictionary_iterator<T,U, MaxSize> const_iterator;
+
+    const_iterator begin() const
+    {
+        return const_iterator(&kvps_);
+    }
+
+    const_iterator end() const
+    {
+        return const_iterator(&kvps_, kvps_.size());
+    }
+
 };
--- src/utility/vector.h
+++ src/utility/vector.h
@@ -8,215 +8,147 @@ template <typename T>
 class vectorIterator
 {
 public:
-    /// @brief
-    /// @param values_ptr
     explicit vectorIterator(T *values_ptr) : values_ptr_{values_ptr}, position_{0} {}
 
-    /// @brief
-    /// @param values_ptr
-    /// @param size
     vectorIterator(T *values_ptr, const size_t size) : values_ptr_{values_ptr}, position_{size} {}
 
-    /// @brief
-    /// @param other
-    /// @return
     bool operator!=(const vectorIterator<T> &other) const
     {
         return !(*this == other);
     }
 
-    /// @brief
-    /// @param other
-    /// @return
     bool operator==(const vectorIterator<T> &other) const
     {
         return position_ == other.position_;
     }
 
-    /// @brief
-    /// @return
     vectorIterator &operator++()
     {
         ++position_;
         return *this;
     }
 
-    /// @brief
-    /// @return
     T &operator*() const
     {
         return *(values_ptr_ + position_);
     }
 
 private:
-    /// @brief points to the holder of the values
     T *values_ptr_;
 
-    /// @brief current element in the values array
     size_t position_;
 };
 
-template <typename T, size_t max_size_ = 10>
+template <typename T, size_t MaxSize = 10>
 class vector
 {
 private:
-    T values_[max_size_];
+    T values_[MaxSize];
 
     size_t size_;
 
 public:
-    /// @brief constructor
     vector()
     {
         size_ = 0;
     }
 
-    /// @brief
-    /// @param index
-    /// @return
     const T &operator[](size_t index) const
     {
         return values_[index];
     }
 
-    /// @brief
-    /// @param index
-    /// @return
     T &operator[](size_t index)
     {
         return values_[index];
     }
 
-    /// @brief
-    /// @param index
-    /// @return
     T &at(size_t index)
     {
         return values_[index];
     }
 
-    /// @brief
-    /// @param index
-    /// @return
     const T &at(size_t index) const
     {
     	return values_[index];
     }
 
-    /// @brief
-    /// @return
     T &front()
     {
         return values_[0];
     }
 
-    /// @brief
-    /// @return
     T &back()
     {
         return values_[size_ - 1];
     }
 
-    /// @brief
     void clear()
     {
         size_ = 0;
     }
 
-    /// @brief
-    /// @param value
     void push_back(const T &value)
     {
-        if ((values_ != nullptr) && (size_ < max_size_))
-        {
+        if ( size_ < MaxSize)
             values_[size_++] = value;
-        }
     }
 
-    /// @brief
     void pop_back()
     {
         if (size_ > 0)
-        {
             --size_;
-        }
     }
 
-    /// @brief
-    /// @return
     size_t size() const
     {
         return size_;
     }
 
-    /// @brief
-    /// @return
     static size_t max_size()
     {
-        return max_size_;
+        return MaxSize;
     }
 
-    /// @brief
-    /// @return
     bool empty() const
     {
         return size_ == 0;
     }
 
-    /// @brief
-    /// @return
     bool full() const
     {
-        return size_ == max_size_;
+        return size_ == MaxSize;
     }
 
-    /// @brief
-    /// @return
     T *data()
     {
         return values_;
     }
 
-    /// @brief
-    /// @return
     const T *data() const
     {
         return values_;
     }
 
-
-
-
-
-    /// @brief
     typedef vectorIterator<T> iterator;
 
-    /// @brief
-    /// @return
     iterator begin()
     {
         return iterator(values_);
     }
 
-    /// @brief
-    /// @return
     iterator end()
     {
         return iterator(values_, size_);
     }
 
-    /// @brief
     typedef vectorIterator<const T> const_iterator;
 
-    /// @brief
-    /// @return
     const_iterator begin() const
     {
         return const_iterator(values_);
     }
 
-    /// @brief
-    /// @return
     const_iterator end() const
     {
         return const_iterator(values_, size_);
