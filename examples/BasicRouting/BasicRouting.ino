00fe698fcb46a0d606716da0d6bdf326a2efe553
lathoub <4082369+lathoub@users.noreply.github.com>
2023-02-18 00:14:13 +0100

    cleanup

--- examples/BasicRouting/BasicRouting.ino
+++ examples/BasicRouting/BasicRouting.ino
@@ -15,15 +15,15 @@ void setup() {
   LOG_SETUP();
 
   ethernet_setup();
-  
 
   app.get("/", [](request &req, response &res, const NextCallback next) {
     res.send("Got a GET request");
   });
 
-  app.post("/", nullptr, [](request &req, response &res, const NextCallback next) {
-    res.send("Got a POST request");
-  });
+  app.post("/", nullptr,
+           [](request &req, response &res, const NextCallback next) {
+             res.send("Got a POST request");
+           });
 
   app.put("/user", [](request &req, response &res, const NextCallback next) {
     res.send("Got a PUT request at /user");
@@ -33,11 +33,7 @@ void setup() {
     res.send("Got a DELETE request at /user");
   });
 
-  app.listen(80, []() {
-    LOG_I(F("Example app listening on port"), app.port);
-  });
+  app.listen(80, []() { LOG_I(F("Example app listening on port"), app.port); });
 }
 
-void loop() {
-  app.run();
-}
+void loop() { app.run(); }
